72dd96516d4542864aa38ddc46ea9708f7d3ba29
wotuthink <suleyman.ahmed@gmail.com>
2024-11-22 18:02:00 +0500

    feat(app1): add new feature

--- apps/app1/index.js
+++ apps/app1/index.js
@@ -4,7 +4,7 @@ const PORT = 3001; // You can choose any port
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to App1!</h1><p>This is running on Express.js. with semantic versioning</p>');
+  res.send('<h1>Welcome to App1!</h1><p>This is running on Express.js. with semantic versioning with new feature</p>');
 });
 
 // Start the server
